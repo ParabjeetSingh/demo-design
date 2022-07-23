a8a4e7b4e6bb005450d8ca56b646c5790904c814
“devParabjeet” <parabjeet@blulabs.in>
2022-07-23 22:39:14 +0530

    demo design added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ImageCont from "./components/imageComp";
+import Navbar from "./components/navbar";
+import TabPanel from "./components/tabs";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <ImageCont />
+      <TabPanel />
     </div>
   );
 }
-
-export default App;
